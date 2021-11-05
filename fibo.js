4865c021335fa7fac5efe768edf2058cdcc7ec10
pablo gonzalez <pgtognoni@gmail.com>
2021-11-04 21:36:47 -0300

    cambio en fibo

--- fibo.js
+++ fibo.js
@@ -1,8 +1,11 @@
 function fiboSeries (number) {
     var fibo = [0, 1];
+    if (number === 1) {
+        return 0;
+    }
     for (var i=fibo.length; i<number; i++) {
      fibo[i] = fibo[i-2] + fibo[i-1];
     }
     return fibo[fibo.length-1];
 }
-console.log(fiboSeries(10));
+console.log(fiboSeries(5));
